d342be71293576894b9976b987fe2b43c797f11f
mkeleti <ladderchair99@gmail.com>
2020-09-18 19:28:22 -0500

    Add files via upload

---
+++ keleti_michael_hw1.py
@@ -0,0 +1,13 @@
+#Loan Calculator by Michael Keleti
+
+
+
+princ = float(input("How much money did you borrow?\n"))
+year = float(input("How many years are required to pay back the loan?\n"))
+frate = float(input("What is the annual interest rate on your loan?\n"))
+mont = year*12
+rate = frate/12
+payment = ((1+rate)**mont * princ * rate)/((1+rate)**mont-1)
+print(payment)
+
+
