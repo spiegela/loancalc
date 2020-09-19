94d656f32c5ba9a92ae94fa39d41504e9f309764
Aaron Spiegel <spiegela@gmail.com>
2020-09-18 19:53:08 -0500

    Update keleti_michael_hw1.py

--- keleti_michael_hw1.py
+++ keleti_michael_hw1.py
@@ -7,6 +7,7 @@ year = float(input("How many years are required to pay back the loan?\n"))
 frate = float(input("What is the annual interest rate on your loan?\n"))
 mont = year*12
 rate = frate/12
+# To change the floating point preceision here you can use printf %.2f: https://stackoverflow.com/questions/7197078/printf-f-with-only-2-numbers-after-the-decimal-point/7197086
 payment = ((1+rate)**mont * princ * rate)/((1+rate)**mont-1)
 print(payment)
 
